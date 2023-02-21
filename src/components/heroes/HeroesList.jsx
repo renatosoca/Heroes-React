2f99cc099bbe7c79d2fff66354bf03ec871a4505
DevRenato <u18215194@gmail.com>
2023-02-20 21:15:22 -0500

    animate

--- src/components/heroes/HeroesList.jsx
+++ src/components/heroes/HeroesList.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
+import React, { useMemo } from 'react'
+import 'animate.css';
+
 import { GetHeroesByPublisher } from '../../helpers/heroes/GetHeroesByPublisher'
 import HeroCard from './HeroCard';
 
 const HeroesList = ( {publisher} ) => {
-  const heroes = GetHeroesByPublisher( publisher );
+  const heroes = useMemo(() => GetHeroesByPublisher( publisher ), [publisher]);
 
   return (
     <>
-      <div className='heroes'>
+      <div className='heroes animate__fadeInDownBig'>
         { heroes.map( hero => <HeroCard key={hero.id} heroe={hero} />) }
       </div>
     </>
--- src/pages/heroes/HeroPage.jsx
+++ src/pages/heroes/HeroPage.jsx
@@ -1,15 +1,42 @@
-import { Navigate, useParams } from "react-router-dom";
+import { useMemo } from "react";
+import { Navigate, useParams, useNavigate } from "react-router-dom";
 import { GetHeroById } from "../../helpers/heroes/GetHeroById";
 
 const HeroPage = () => {
+  const navigate = useNavigate();
   const { heroId } = useParams();
-  const hero = GetHeroById(heroId);
+  
+  const hero = useMemo( () => GetHeroById(heroId), [ heroId ] );
+
+  const handleReturn = () => {
+    navigate(-1);
+  }
   
   if ( !hero ) return <Navigate to="/dc" />;
 
   return (
     <>
-      
+      <div className="heroe">
+        <div className="heroe__container">
+          <div className="heroe__img">
+            <img src={`/assets/img/${ heroId }.jpg`} alt="" />
+          </div>
+
+          <div className="heroe__content">
+            <h1 className="heroe__title">{ hero.superhero }</h1>
+            <p className="heroe__text">{ hero.alter_ego }</p>
+            <p className="heroe__text">{ hero.first_appearance }</p>
+            <p className="heroe__text">{ hero.publisher }</p>
+
+            <button
+              className="heroe__button"
+              onClick={ handleReturn }
+            >
+              Regresar
+            </button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
