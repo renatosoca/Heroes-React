ce6ef866ca765466a8f5236809329fa452a0bc9a
DevRenato <u18215194@gmail.com>
2023-02-20 20:43:59 -0500

    useParams

--- src/pages/heroes/HeroPage.jsx
+++ src/pages/heroes/HeroPage.jsx
@@ -1,10 +1,16 @@
-
+import { Navigate, useParams } from "react-router-dom";
+import { GetHeroById } from "../../helpers/heroes/GetHeroById";
 
 const HeroPage = () => {
+  const { heroId } = useParams();
+  const hero = GetHeroById(heroId);
   
+  if ( !hero ) return <Navigate to="/dc" />;
 
   return (
-    <div>HeroPage</div>
+    <>
+      
+    </>
   )
 }
 
